f67f53bfce1fc3bf7abe947906de7d4d85b33309
Manuel <manueldavidb@mail.afeka.ac.il>
2015-03-23 23:20:54 +0200

    Account Message Works

--- SNMS_Client/MainWindow.xaml.cs
+++ SNMS_Client/MainWindow.xaml.cs
@@ -27,7 +27,12 @@ namespace SNMS_Client
     public partial class MainWindow : Window
     {
         const int TCP_PORT = 56824;
+
         static List<Plugin> pluginList;
+        static List<Account> accountList;
+
+        static TcpClient client;
+        static NetworkStream stream;
 
         public MainWindow()
         {
@@ -37,35 +42,72 @@ namespace SNMS_Client
             //Available_Plugins;
         }
 
+        bool ConnectionProcedure()
+        {
+            ProtocolMessage connectionMessage = new ProtocolMessage();
+            connectionMessage.SetMessageType(ProtocolMessageType.PROTOCOL_MESSAGE_CONNECTION);
+            connectionMessage.AddParameter("client");
+
+            client = new TcpClient();
+            client.Connect("127.0.0.1", TCP_PORT);
+
+            stream = client.GetStream();
+
+            ConnectionHandler.SendMessage(stream, connectionMessage);
+            ProtocolMessage responseMessage = ConnectionHandler.GetMessage(stream);
+
+            return true;
+        }
+
+        bool LoadPluginsTab()
+        {
+            Plugins_Available_Plugins.Items.Clear();
+            
+            ProtocolMessage pluginsMessage = new ProtocolMessage();
+            pluginsMessage.SetMessageType(ProtocolMessageType.PROTOCOL_MESSAGE_GET_PLUGINS);
+
+            ConnectionHandler.SendMessage(stream, pluginsMessage);
+            ProtocolMessage responseMessage = ConnectionHandler.GetMessage(stream);
+
+            pluginList = Plugin.ParseMessage(responseMessage);
+            foreach (Plugin plugin in pluginList)
+            {
+                Plugins_Available_Plugins.Items.Add(plugin.GetName());
+            }
+
+            return true;
+        }
+
+        bool LoadAccountsTab()
+        {
+            Accounts_Available_Plugins.Items.Clear();
+            Account_ComboBox.SelectedIndex = -1;
+
+            foreach (Plugin plugin in pluginList)
+            {
+                Accounts_Available_Plugins.Items.Add(plugin.GetName());
+            }
+            return true;
+        }
+
+        bool LoadConfigurationsTab()
+        {
+            Accounts_Available_Plugins.Items.Clear();
+            Account_ComboBox.SelectedIndex = -1;
+
+            foreach (Plugin plugin in pluginList)
+            {
+                Accounts_Available_Plugins.Items.Add(plugin.GetName());
+            }
+            return true;
+        }
+
         bool LoadStartValues()
         {
             try
             {
-                ProtocolMessage connectionMessage = new ProtocolMessage();
-                connectionMessage.SetMessageType(ProtocolMessageType.PROTOCOL_MESSAGE_CONNECTION);
-                connectionMessage.AddParameter("client");
-
-                ProtocolMessage pluginsMessage = new ProtocolMessage();
-                pluginsMessage.SetMessageType(ProtocolMessageType.PROTOCOL_MESSAGE_GET_PLUGINS);
-
-                TcpClient client = new TcpClient();
-                client.Connect("127.0.0.1", TCP_PORT);
-
-                Stream stream = client.GetStream();
-
-                //byte[] response = Protocol.CraftMessage(pluginsMessage);
-                //stream.Write(response, 0, response.Length);
-                //stream.Flush();
-                ConnectionHandler.SendMessage(stream, connectionMessage);
-                ProtocolMessage responseMessage = ConnectionHandler.GetMessage(stream);
-                ConnectionHandler.SendMessage(stream, pluginsMessage);
-                responseMessage = ConnectionHandler.GetMessage(stream);
-
-                pluginList = Plugin.ParseMessage(responseMessage);
-                foreach (Plugin plugin in pluginList)
-                {
-                    Available_Plugins.Items.Add(plugin.GetName());
-                }
+                ConnectionProcedure();
+                //LoadPluginsTab();
             }
             catch (Exception e)
             {
@@ -75,9 +117,9 @@ namespace SNMS_Client
             return true;
         }
 
-        private void Available_Plugins_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        private void Plugins_Available_Plugins_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            if (Available_Plugins.SelectedIndex == -1)
+            if (Plugins_Available_Plugins.SelectedIndex == -1)
             {
                 Plugin_Name.Text = "";
                 Plugin_Description.Text = "";
@@ -85,7 +127,7 @@ namespace SNMS_Client
             }
             else
             {
-                Plugin plugin = pluginList[Available_Plugins.SelectedIndex];
+                Plugin plugin = pluginList[Plugins_Available_Plugins.SelectedIndex];
                 Plugin_Name.Text = plugin.GetName();
                 Plugin_Description.Text = plugin.GetDescription();
                 bool isEnabled = plugin.GetEnabled();
@@ -98,6 +140,79 @@ namespace SNMS_Client
                     Plugin_Enable_Button.Content = "Disabled";
                 }
                 
+            }
+        }
+
+        private void Accounts_Available_Plugins_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Account_ComboBox.Items.Clear();
+
+            Account_ComboBox.SelectedIndex = -1;
+            Account_Name.Text = "";
+            Account_Description.Text = "";
+            Account_User_Name.Text = "";
+            Account_Password.Password = "";
+
+            if (Accounts_Available_Plugins.SelectedIndex != -1)
+            {
+                int dwIndex = Accounts_Available_Plugins.SelectedIndex;
+                Plugin plugin = pluginList[dwIndex];
+                int dwPluginID = plugin.GetID();
+
+                ProtocolMessage accountsMessage = new ProtocolMessage();
+                accountsMessage.SetMessageType(ProtocolMessageType.PROTOCOL_MESSAGE_GET_ACCOUNTS);
+
+                accountsMessage.AddParameter(dwPluginID);
+
+                ConnectionHandler.SendMessage(stream, accountsMessage);
+                ProtocolMessage responseMessage = ConnectionHandler.GetMessage(stream);
+
+                accountList = Account.ParseMessage(responseMessage, plugin);
+
+                foreach (Account account in accountList)
+                {
+                    Account_ComboBox.Items.Add(account.GetName());
+                }
+            }
+        }
+
+        private void Account_ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            int index = Account_ComboBox.SelectedIndex;
+            if ( index == -1)
+            {
+                Account_Name.Text = "";
+                Account_Description.Text = "";
+                Account_User_Name.Text = "";
+                Account_Password.Password = "";
+            }
+            else
+            {
+                Account account = accountList[index];
+                Account_Name.Text = account.GetName();
+                Account_Description.Text = account.GetDescription();
+                Account_User_Name.Text = account.GetUserName();
+                Account_Password.Password = account.GetPassword();
+            }
+        }
+
+        private void Main_Tab_Controller_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (e.AddedItems.Count > 0 && e.AddedItems[0] is TabItem)
+            {
+                switch (Main_Tab_Controller.SelectedIndex)
+                {
+                    case 0:
+                        LoadPluginsTab();
+                        break;
+
+                    case 1:
+                        LoadAccountsTab();
+                        break;
+
+                    default:
+                        break;
+                }
             }
         }
     }
